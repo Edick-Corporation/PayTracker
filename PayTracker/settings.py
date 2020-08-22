6ed38dc7b891092a8d8e1822f506177abf900417
thisisfuckingalex <alxogloblin@gmail.com>
2020-08-22 07:09:33 +0300

    moved .html to templates, .css to static, created Purchases model and db

--- PayTracker/settings.py
+++ PayTracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Purchase(models.Model):
+    type = models.CharField('Тип покупки', max_length=100)
+    cost = models.PositiveIntegerField('Стоимость')
+    slug = models.SlugField('Url', max_length=110, unique=True)
+    degree_of_importance = models.SmallIntegerField('Степень важности', default=0)
+    date = models.DateTimeField('Когда была сделана покупка', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Покупка'
+        verbose_name_plural = 'Покупки'
+        ordering = ['date']
+
+    def __str__(self):
+        return self.type
+
+    def get_absolute_url(self):
+        return reverse('purchase_detail', kwargs={'purchase_pk': self.pk})
