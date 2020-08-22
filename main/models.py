9be545cf14a10cb56869ab802f2641d2fecf4ee1
thisisfuckingalex <alxogloblin@gmail.com>
2020-08-22 15:38:05 +0300

    created new model Type

--- main/models.py
+++ main/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 from django.urls import reverse
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Type(MPTTModel):
+    name = models.CharField('Тип покупки', max_length=100)
+    slug = models.SlugField('Url', max_length=110)
+    parent = TreeForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='children'
+    )
+
+    class Meta:
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('type_detail', kwargs={'type_slug': self.slug})
 
 
 class Purchase(models.Model):
-    type = models.CharField('Тип покупки', max_length=100)
+    type = models.ForeignKey(Type, related_name='Type', on_delete=models.CASCADE)
     cost = models.PositiveIntegerField('Стоимость')
     slug = models.SlugField('Url', max_length=110, unique=True)
     degree_of_importance = models.SmallIntegerField('Степень важности', default=0)
@@ -18,4 +42,4 @@ class Purchase(models.Model):
         return self.type
 
     def get_absolute_url(self):
-        return reverse('purchase_detail', kwargs={'purchase_pk': self.pk})
+        return reverse('purchase_detail', kwargs={'type_slug': self.type.slug, 'purchase_pk': self.pk})
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
 # fuck you
 # СЛЫШ ты лучше успокой свой член лады?
@@ -7,3 +7,5 @@ from django.shortcuts import render
 from main.models import Purchase
 
 
+class PurchaseList(ListView):
+    model = Purchase
