a8eac141e6c4d937bb8223c61f7f4e5ed7739176
thisisfuckingalex <alxogloblin@gmail.com>
2020-08-22 07:20:34 +0300

    added app and created model in

---
+++ statistics_m/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StatisticsMConfig(AppConfig):
+    name = 'statistics_m'
---
+++ statistics_m/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.urls import reverse
+
+from main.models import Purchase
+
+
+class Statistics(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    slug = models.SlugField('Url', max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Статистика'
+        verbose_name_plural = 'Статистки'
+
+    def __str__(self):
+        return str(self.pk)
+
+    def get_absolute_url(self):
+        return reverse('statistics_detail', kwargs={'statistics_pk': self.pk})
